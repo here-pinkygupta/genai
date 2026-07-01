a0c16ea6463fbbf11ea450dd053dcfaae370539b
pinkyy <181976501+here-pinkygupta@users.noreply.github.com>
2026-07-01 10:26:00 +0000

    Add CORS origin logging

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,26 +3,32 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-
 
 const allowedOrigins = [
-    "https://resume-analyzer-puce-eight.vercel.app",
-    "https://resume-analyzer-4s2muzf6j-here-pinkyguptas-projects.vercel.app",
+  "http://localhost:5173",
+  "https://resume-analyzer-oqswykkcf-here-pinkyguptas-projects.vercel.app"
 ];
 
 app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
+  origin: function (origin, callback) {
+    console.log("🌍 Request Origin:", origin);
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      console.log("✅ Origin Allowed");
+      callback(null, true);
+    } else {
+      console.log("❌ Blocked Origin:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
 app.get("/test", (req, res) => {
     res.json({ message: "Backend is working" });
 });
