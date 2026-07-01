94ea4d7cb8b2aa86045605d26347064fc7ba0021
pinkyy <181976501+here-pinkygupta@users.noreply.github.com>
2026-07-01 11:27:31 +0000

    debug render build

--- Backend/render-build.sh
+++ Backend/render-build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
+echo "===== RENDER BUILD START ====="
 
 npm install
 
+echo "Installing Chrome..."
 npx puppeteer browsers install chrome
+
+echo "===== RENDER BUILD FINISHED ====="
