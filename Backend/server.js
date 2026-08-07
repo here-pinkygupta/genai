46df3356f8d310155d8f41b0b6e36b0e0e90210a
pinkyy <181976501+here-pinkygupta@users.noreply.github.com>
2026-08-07 13:05:18 +0000

    Ready for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,8 @@ const connectToDb = require('./src/config/database')
 
 connectToDb()
 
-app.listen(3000, () => {
-    console.log('server connected on 3000')
-})
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
